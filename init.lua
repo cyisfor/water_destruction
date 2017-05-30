b1f985ab516ebd6dca7071f045e988d5907d6187
user <nobody>
2017-05-30 02:35:05 +0000

    doooom

---
+++ init.lua
@@ -0,0 +1,32 @@
+-- stolen from builtin/game/falling.lua which made it a local function too.
+local function drop(p)
+        local nn = core.get_node(p).name
+        core.remove_node(p)
+        for _, item in pairs(core.get_node_drops(nn, "")) do
+                local pos = {
+                        x = p.x + math.random()/2 - 0.25,
+                        y = p.y + math.random()/2 - 0.25,
+                        z = p.z + math.random()/2 - 0.25,
+                }
+                core.add_item(pos, item)
+        end
+end
+
+local abm = {
+			label = "Destroy things",
+			nodenames = {"group:water"},
+			neighbors = {"group:plant","group:flora","group:torch"},
+			catch_up = false,
+			interval = 3,
+			chance = 1,
+			action = function(pos, node, count, count_wider)
+				 local minp = {x=pos.x-1,y=pos.y-1,z=pos.z-1}
+				 local maxp = {x=pos.x+1,y=pos.y+1,z=pos.z+1}
+				 local ns = core.find_nodes_in_area(minp,maxp,abm.neighbors)
+				 for _,pos in ipairs(ns) do
+						drop(pos)
+				 end
+			end						
+}
+
+core.register_abm(abm)
